fd4781a571fc6c85648d6c8db56c173f096ca2b3
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2024-11-23 18:35:29 -0500

    Add volunteer page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,18 +4,19 @@ const colors = require("tailwindcss/colors");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: colors.black,
-      white: colors.white,
-      gray: "#EEEEEC",
-      red: colors.rose,
-      yellow: colors.amber,
-      blue: "#007BC1",
-      orange: "#FF9900",
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        gray: "#EEEEEC",
+        red: colors.rose,
+        yellow: colors.amber,
+        "bot-blue": "#007BC1",
+        "bot-orange": "#FF9900",
+      },
     },
-    extend: {},
     fontFamily: {
       heading: ["Roboto", "sans-serif"],
       paragraph: ["Montserrat", "sans-serif"],
